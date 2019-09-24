51fc631ce8bb7b5fe7cfe7f548fa85c1d106b7dd
Gustavo Henrique Dias <31208960+GustavoDiias@users.noreply.github.com>
2019-09-23 23:59:48 -0300

    Tela 6 - Perfil de Usuario

--- src/index.js
+++ src/index.js
@@ -3,8 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import TelaModoDeUsar from './TelaModoDeUsar';
+import TelaPerfil from './TelaPerfil';
+import TelaSejaBemVindoCliente from './TelaSejaBemVindoCliente';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<TelaSejaBemVindoCliente />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
