1b6c7a05be8fbddbea12e658a8129147dccc857b
Wender Lucas <wenderlucas.souza@hotmail.com>
2019-11-26 20:09:37 -0300

    Update Notificar.js

--- src/pages/Padaria/Notificar.js
+++ src/pages/Padaria/Notificar.js
@@ -79,7 +79,7 @@ export default class Notificar extends React.Component{
                                                 <Form.Control type="text" placeholder="Texto da notificação" name="texto" value={this.state.value} onChange={this.handleChange}/>
                                             </Form.Group>
 
-                                            <Button style={{color: "blue"}}> <AddAlertIcon/> Notificar</Button>
+                                            <Button type="submit" style={{color: "blue"}}> <AddAlertIcon/> Notificar</Button>
                                         </Form>  
                                     </Col>                                 
                                     </Col>
